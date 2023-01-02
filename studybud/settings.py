97a27425166a07cc5711e2bde4fe5f43e8551f0a
noproblem2311 <quan2311@gmail.com>
2023-01-02 20:23:18 +0700

    first commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = '/images/' 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
